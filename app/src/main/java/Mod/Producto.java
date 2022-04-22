47dceb4ca776c52ef3d151bbac320612e8d55af5
Jose Murillo <97691714+Danijose2002murillo@users.noreply.github.com>
2022-04-23 03:15:58 +0800

    cambio

---
+++ app/src/main/java/Mod/Producto.java
@@ -0,0 +1,150 @@
+package Mod;
+
+import android.content.Context;
+import android.database.Cursor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Producto {
+
+    private int codigo;
+    private String nombre;
+    private String descripcion;
+    private double costo;
+    private double precio;
+    private int stock;
+    private String fecha;
+    private byte imagen;
+
+    public Producto(int codigo, String nombre, String descripcion, double costo, double precio, int stock, String fecha, byte imagen) {
+        this.codigo = codigo;
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+        this.costo = costo;
+        this.precio = precio;
+        this.stock = stock;
+        this.fecha = fecha;
+        this.imagen = imagen;
+    }
+
+    private List<Producto> lista=new ArrayList<>();
+
+    public Producto() {
+    }
+
+    public void guardar(Context context) {
+        DbHelper db = new DbHelper(context);
+        System.out.println("hola");
+        String noSql = "INSERT INTO producto (id,nombre,descripcion,costo,precio,stock,fecha) " +
+                "VALUES (" + getCodigo() + ",'" + getNombre()+ "','" + getDescripcion() + "'," + getCosto()+ ","+getPrecio()+","
+                +getStock()+",'"+getFecha()+"');";
+        db.noQuery(noSql);
+        db.close();
+    }
+
+    public void Editar(Context context) {
+        DbHelper db = new DbHelper(context);
+      /*  String noSql = "UPDATE producto set nombre= ' "+getNombre()+"', descripcion= '"+getDescripcion()
+                + "', costo= "+getCosto()+", precio= "+getPrecio()+", stock= "+getStock()+ " where id="+getCodigo()+";";*/
+
+       String noSql = "UPDATE producto set nombre= ' "+getNombre()+"', descripcion= '"+getDescripcion()
+                + "', costo= "+getCosto()+", precio= "+getPrecio()+", stock= "+getStock()+ ", fecha= '"+getFecha()+ " '  where id="+getCodigo()+";";
+        db.noQuery(noSql);
+        db.close();
+    }
+
+    public void eliminar(Context context){
+        DbHelper db = new DbHelper(context);
+        String noSql = "DELETE FROM producto where id= "+getCodigo();
+        db.noQuery(noSql);
+        db.close();
+
+    }
+
+
+
+    public List<Producto> listar(Context context) {
+        DbHelper db = new DbHelper(context);
+        String sql = "SELECT * FROM producto";
+        Cursor cursor = db.query(sql);
+        while(cursor.moveToNext()){
+            Producto p=new Producto();
+            p.setCodigo(cursor.getInt(0));
+            p.setNombre(cursor.getString(1));
+            p.setDescripcion(cursor.getString(2));
+            p.setCosto(cursor.getDouble(4));
+            p.setPrecio(cursor.getDouble(3));
+            p.setStock(cursor.getInt(5));
+            p.setFecha(cursor.getString(6));
+            lista.add(p);
+        }
+        return lista;
+    }
+
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public double getCosto() {
+        return costo;
+    }
+
+    public void setCosto(double costo) {
+        this.costo = costo;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+    public String getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(String fecha) {
+        this.fecha = fecha;
+    }
+
+    public byte getImagen() {
+        return imagen;
+    }
+
+    public void setImagen(byte imagen) {
+        this.imagen = imagen;
+    }
+}
--- app/src/main/java/com/example/examencaso3/MainActivity.java
+++ app/src/main/java/com/example/examencaso3/MainActivity.java
@@ -1,13 +1,188 @@
 package com.example.examencaso3;
 
-import androidx.appcompat.app.AppCompatActivity;
+import Mod.DbHelper;
+import Mod.Producto;
+import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.*;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    ProgressBar progressBar;
+    Button btneliminar;
+    Button btnEditar;
+    Button btnGrabar;
+    EditText nombre,id,descripcion,costo,precio,stock,fecha;
+    ArrayList<String> datos;
+    Producto prod = new Producto();
+    List<Producto> lista;
+    ListView listViewcliente;
+    ArrayAdapter arrayAdapter ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        progressBar=(ProgressBar) findViewById(R.id.progressBar);
+        ImageButton buttonCamara=findViewById(R.id.buttonCamara);
+        progressBar.setVisibility(View.INVISIBLE);
+        new Hilo1().start();
+
+
+        DbHelper dbHelper = new DbHelper(MainActivity.this);
+        SQLiteDatabase db = dbHelper.getReadableDatabase();
+        if (db != null) {
+            Toast.makeText(getApplicationContext(), "se creo la base de datos", Toast.LENGTH_LONG).show();
+        } else {
+            Toast.makeText(getApplicationContext(), "Error de creacion ", Toast.LENGTH_LONG).show();
+        }
+
+        listar();
+
+        listViewcliente = (ListView) findViewById(R.id.list_view);
+        arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, datos);
+        listViewcliente.setAdapter(arrayAdapter);
+
+        buttonCamara.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentCamara= new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+
+                if(intentCamara.resolveActivity(getPackageManager())!=null){
+
+                    startActivityForResult(intentCamara,1);
+                }
+            }
+        });
+
+        btnGrabar = findViewById(R.id.btn_grabar);
+        btnEditar = findViewById(R.id.btn_editar);
+        btneliminar = findViewById(R.id.btn_eliminar);
+        nombre=findViewById(R.id.txt_Nombre);
+        id=findViewById(R.id.txt_id);
+        descripcion=findViewById(R.id.txt_descripcion);
+        costo=findViewById(R.id.txt_costo);
+        precio=findViewById(R.id.txt_precio);
+        stock=findViewById(R.id.txt_stock);
+        fecha=findViewById(R.id.txt_fecha);
+
+        btnGrabar.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        guardarDatos(1);
+
+                    }
+                }
+
+        );
+
+        btnEditar.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        guardarDatos(2);
+
+                    }
+                }
+        );
+
+        btneliminar.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        eliminar();
+
+                    }
+                }
+        );
+
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode==1 && resultCode==RESULT_OK){
+            Bundle bundle =data.getExtras();
+            Bitmap imagen= (Bitmap) bundle.get("data");
+            ImageView imageView=findViewById(R.id.imageView);
+            imageView.setImageBitmap(imagen);
+        }
+
+    }
+
+    class Hilo1 extends Thread {
+        @Override
+        public void run() {
+            try {
+                Thread.sleep(5000);
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        progressBar.setVisibility(View.INVISIBLE);
+                    }
+                });
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+
+
+    public void guardarDatos(int op) {
+        Producto product = new Producto();
+
+        product.setCodigo(Integer.parseInt(id.getText().toString()));
+        product.setNombre(nombre.getText().toString());
+        product.setDescripcion(descripcion.getText().toString());
+        product.setCosto(Double.parseDouble(costo.getText().toString()));
+        product.setPrecio(Double.parseDouble(precio.getText().toString()));
+        product.setStock(Integer.parseInt(stock.getText().toString()));
+        product.setFecha(fecha.getText().toString());
+
+        if (op == 1) {
+            product.guardar(MainActivity.this);
+            Toast.makeText(getApplicationContext(), "se creo el producto".toString(), Toast.LENGTH_LONG).show();
+
+        } else if (op == 2) {
+            product.Editar(MainActivity.this);
+            Toast.makeText(getApplicationContext(), "editando producto".toString(), Toast.LENGTH_LONG).show();
+        }
+    }
+
+
+    public void eliminar() {
+        Producto product = new Producto();
+        product.setCodigo(Integer.parseInt(id.getText().toString()));
+        product.eliminar(MainActivity.this);
+        Toast.makeText(getApplicationContext(), "se elimino el producto".toString(), Toast.LENGTH_LONG).show();
+
+    }
+
+
+    public void listar() {
+        datos = new ArrayList<String>();
+        lista = prod.listar(MainActivity.this);
+
+        for (int i = 0; i < lista.size(); i++) {
+            datos.add("CODIGO: "+lista.get(i).getCodigo()+" NOMBRE: "+lista.get(i).getNombre()+" DESCRIPCION: "+lista.get(i).getDescripcion()
+                    +" COSTO: "+ lista.get(i).getCosto()+" PRECIO: "+lista.get(i).getPrecio()+" STOCK: "+
+                    lista.get(i).getStock()+" FECHA: "+lista.get(i).getFecha());
+        }
+    }
+
+
+
+
 }
